96739e0dd3cbb3baa9b199dcded79e37805f6d5e
ARMilon927 <a.r.milon927@gmail.com>
2019-10-15 00:23:24 +0600

    Practice 1 is done

---
+++ AccountOperationAppPractice1/AccountOperationAppPractice1/AccountOperationUi.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountOperationAppPractice1
+{
+    public partial class AccountOperationUi : Form
+    {
+        public AccountOperationUi()
+        {
+            InitializeComponent();
+        }
+        Customer aCustomer = new Customer();
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+            aCustomer.customerName = customerNameTextBox.Text;
+            aCustomer.accountName = accountNoTextBox.Text;
+        }
+
+        private void DipositButton_Click(object sender, EventArgs e)
+        {
+            double amount = Convert.ToDouble(amountTextBox.Text);
+            aCustomer.Deposit(amount);
+        }
+
+        private void WithdrawButton_Click(object sender, EventArgs e)
+        {
+            double amount = Convert.ToDouble(amountTextBox.Text);
+            aCustomer.Withdraw(amount);
+        }
+
+        private void ReportButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(aCustomer.Report());
+        }
+    }
+}
---
+++ AccountOperationAppPractice1/AccountOperationAppPractice1/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountOperationAppPractice1
+{
+    class Customer
+    {
+        public string accountName;
+        public string customerName;
+        public double balance = 0;
+
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+        public void Withdraw(double amount)
+        {
+            balance -= amount;
+        }
+
+        public string Report()
+        {
+            string message;
+            message = customerName + " , your account Number : " + accountName + " and it's balance : " + balance;
+            return message;
+        }
+    }
+}
